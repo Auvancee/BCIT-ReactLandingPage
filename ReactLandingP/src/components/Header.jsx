3d3d536fcb493a69ef6ce73da0cd2cfffb980d0c
Aakif Umar <therealauvance@gmail.com>
2025-03-11 03:30:41 -0700

    - Designed Header in respect to the design

--- ReactLandingP/src/components/Header.jsx
+++ ReactLandingP/src/components/Header.jsx
@@ -1,75 +1,53 @@
-// import { APP_TITLE } from "../globals/global";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-// import useHamburgerMenu from "../globals/hamburgermenu";
-
-import "./Header.css"
-// import logo from "../assets/logo.svg";
-// import user from "../assets/User.svg";
-
-
+import "./Header.css";
 
 function Header() {
-    // const { isMenuOpen, toggleMenu } = useHamburgerMenu() || {};
+  const [isOpen, setIsOpen] = useState(false);
+  const [isHovering, setIsHovering] = useState(false);
 
+  const toggleMenu = () => setIsOpen((prev) => !prev);
+  const handleMouseEnter = () => setIsHovering(true);
+  const handleMouseLeave = () => setIsHovering(false);
 
-    return (
-        // <!-- [1] Header & Navigation (GLOBAL)  -->
-        <header>
-            <nav>
-                {/* <Link to="/home">
-                    <img src={logo} className="ona-film-logo" alt="uae-flag-logo" />
-                </Link> */}
-                
-                <div className="NavConatainer">
-                    <div className="TextLogo">
-                        <Link to="https://aumar.xyz">@HAFIZAU</Link>
-                    </div>
+  return (
+    <header className="header-container">
+      <nav className="nav-bar">
+        {/* Left Logo */}
+        <div className="text-logo">
+          <Link to="https://aumar.xyz">@HAFIZAU</Link>
+        </div>
 
-                    <div className="NavMenu">
-                        <ul>
-                            <p>MENU</p>
-                            {/* <li><Link to="/">Home</Link></li> */}
-                            <li><Link to="#About">ABOUT</Link></li>
-                            <li><Link to="#Featured">FEATURED</Link></li>
-                            <li><Link to="../pages/PageSynopsis.jsx">SYNOPSIS</Link></li>
-                            <li><Link to="../pages/PageWorks.jsx.jsx">PROJECTS</Link></li>
-                            <li><Link to="#Contact">CONTACT</Link></li>
-                            {/* <Link to="/login"><img src={user} className="UserSvg" alt="user-login" /></Link> */}
-                            {/* <li><Link to="/movie/1">Movie</Link></li> */}
-                        </ul>
-                    </div>
-                </div>
-                
+        {/* Right Menu with Hover + Toggle */}
+        <div
+          className="menu-toggle"
+          onClick={toggleMenu}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          
+          {/* Arrow appears on hover or when menu is open */}
+          {(isHovering || isOpen) && (
+            <span className={`arrow ${isOpen ? "rotate" : ""}`}>←</span>
+          )}
+          <span className="menu-label">MENU</span>
 
-                
-                
-
-                {/* <!-- [1.1] Hamburger Menu  --> */}
-                {/* <!-- Codepen Link: https://codepen.io/ainalem/pen/LJYRxz --> */}
-
-                {/* <svg id="hamburger-menu" className={`ham hamRotate ham4 ${isMenuOpen ? 'active' : ''}`} viewBox="0 0 100 100" width="60" onClick={toggleMenu}>
-                    <path className="line top" d="m 70,33 h -40 c 0,0 -8.5,-0.149796 -8.5,8.5 0,8.649796 8.5,8.5 8.5,8.5 h 20 v -20" />
-                    <path className="line bottom" d="m 30,67 h 40 c 0,0 8.5,0.149796 8.5,-8.5 0,-8.649796 -8.5,-8.5 -8.5,-8.5 h -20 v 20" />
-                </svg> */}
-
-
-            </nav>
-
-            
-
-            {/* <!-- [1.2] Overlay (w/ Hamburger Onclick)  --> */}
-            {/* <div className={`overlay ${isMenuOpen ? 'active' : ''}`} id="menuOverlay"> */}
-                {/* <div className="overlay-content">
-                    <ul>
-                        <li className="menu-item"><Link to="/login">Login</Link></li>
-                        <li className="menu-item"><Link to="/home">Home</Link></li>
-                        <li className="menu-item"><Link to="/favourites">Favourites</Link></li>
-                        <li className="menu-item"><Link to="/about">About</Link></li>
-                        
-                    </ul>
-                </div> */}
-            {/* </div> */}
-        </header>
-    );
+          {/* Dropdown Menu */}
+          {isOpen && (
+            <div className="dropdown-menu">
+              <ul>
+                <li><Link to="#About" onClick={toggleMenu}>ABOUT</Link></li>
+                <li><Link to="#Featured" onClick={toggleMenu}>FEATURED</Link></li>
+                <li><Link to="/pages/PageSynopsis" onClick={toggleMenu}>SYNOPSIS</Link></li>
+                <li><Link to="/pages/PageWorks" onClick={toggleMenu}>PROJECTS</Link></li>
+                <li><Link to="#Contact" onClick={toggleMenu}>CONTACT</Link></li>
+              </ul>
+            </div>
+          )}
+        </div>
+      </nav>
+    </header>
+  );
 }
+
 export default Header;
