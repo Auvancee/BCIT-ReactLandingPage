1559ccf5b21099f59954a297a802f7f457c995ce
Aakif Umar <therealauvance@gmail.com>
2025-04-05 00:10:11 -0700

    - Added GSAP overlaying Cards

--- ReactLandingP/src/pages/PageSynopsis.jsx
+++ ReactLandingP/src/pages/PageSynopsis.jsx
@@ -11,12 +11,26 @@ import SplideDiamond2 from "../components/images/DiamondSplide.svg"
 import Profile1 from "../components/images/Portrait1.png";
 import Profile2 from "../components/images/Portrait2.png";
 
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import ScrollToPlugin from "gsap/ScrollToPlugin";
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
+import initScrollAnimation2 from "../global/projectscroll2"; // Desktop GSAP
+
 
 
 function PageSynopsis() {
 
     const containerRef = useRef(null);
         const lenis = useLenis(({ scroll }) => {});
+
+    useEffect(() => {
+        let ctx = gsap.context(() => {
+            initScrollAnimation2();
+        }, containerRef);
+        
+        return () => ctx.revert(); // Clean up animation on unmount
+    }, []);
     
         useEffect(() => {
             initializeSplide();
@@ -61,6 +75,8 @@ function PageSynopsis() {
                             <h3><span className="SHighLight1">DEVELOPER <br/></span><span className="SHighLight2">DESIGNER &</span> STUDENT</h3>
                         </div>
 
+                    
+
                         <div className="LeftColumn">
                             <img src={Profile1} alt="" />
                             <p>A young student captivated by the digital space, 
@@ -96,10 +112,108 @@ function PageSynopsis() {
                             <h3>FAMILIAR WITH WHAT I DO</h3>
                         </div>
 
+                        <div className="SynopsisContent">
+                            <p>Meticulosly crafted projects that have been designed, developed
+                                & reviewed from the absolute bare bone. 
+                            </p>
+                        </div>
+
+                        {/* Skillset Cards */}
+                        <div className="cardskillset">
+
+                            <div className="cardskill card-1">
+                                <div className="card-contentskill">
+
+                                <div className="cardskill-info">
+                                    <h3>Search Optimization SEO</h3>
+                            
+                                    <p>
+                                    Amplify your online presence with a website that truly connects with your audience's feelings and desires. I design stunning, high-converting sites that align with your business goals.
+                                    </p>
+                            
+                                    <div className="cardskill-tags">
+                                    <span>CMS Integration</span>
+                                    <span>Motion & Animate</span>
+                                    <span>3D Development</span>
+                                    </div>
+                                </div>
+                            
+                                {/* <!-- Hexagonal Icon (Right Side) --> */}
+                                <div className="card-icon">
+                                    {/* <!-- Insert Icon or SVG here --> */}
+                                </div>
+
+                                </div>
+                            </div>
+
+                            <div className="cardskill card-2">
+                                <div className="card-contentskill">
+
+                                <div className="cardskill-info">
+                                    <h3>Search Optimization SEO</h3>
+                            
+                                    <p>
+                                    Amplify your online presence with a website that truly connects with your audience's feelings and desires. I design stunning, high-converting sites that align with your business goals.
+                                    </p>
+                            
+                                    <div className="cardskill-tags">
+                                    <span>CMS Integration</span>
+                                    <span>Motion & Animate</span>
+                                    <span>3D Development</span>
+                                    </div>
+                                </div>
+                            
+                                {/* <!-- Hexagonal Icon (Right Side) --> */}
+                                <div className="card-icon">
+                                    {/* <!-- Insert Icon or SVG here --> */}
+                                </div>
+
+                                </div>
+                            </div>
+
+                            <div className="cardskill card-3">
+                                <div className="card-contentskill">
+                                
+                                    <div className="cardskill-info">
+                                    <h3>Search Optimization SEO</h3>
+
+                                    <p>
+                                        Amplify your online presence with a website that truly connects with your audience's feelings and desires. I design stunning, high-converting sites that align with your business goals.
+                                    </p>
+
+                                    <div className="cardskill-tags">
+                                        <span>CMS Integration</span>
+                                        <span>Motion & Animate</span>
+                                        <span>3D Development</span>
+                                    </div>
+                                    </div>
+
+                                    {/* <!-- Hexagonal Icon (Right Side) --> */}
+                                    <div className="card-icon">
+                                    {/* <!-- Insert Icon or SVG here --> */}
+                                    </div>
+
+                                </div>
+                            </div>
+
+                        </div>
+                        
+
+
+
+
                     </article>
 
-                {/* <FooterTwo/>  
-                <br/><br/> */}
+
+                    {/* CTA Section */}
+                    <div className="CTABeforeTwo">
+                        <h4>READY TO</h4>
+                        <h5>TRANSFORM</h5>
+                        <h4>YOUR EXPERIENCE?</h4>
+                    </div>
+
+                <FooterTwo/>  
+                <br/><br/>
             </section>
         
         </main>
